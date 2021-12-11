5d8fbd5af38cd67b707b0db49825b001b8f805de
xKamiZ <officialdarkshadows@gmail.com>
2021-12-11 22:56:06 +0100

    Movimiento prematuro boss

---
+++ Assets/Scripts/Enemies/Boss/AI_BossBasicShoot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_BossBasicShoot : MonoBehaviour
+{
+	#region VARIABLES
+	[SerializeField] private Transform m_shootPoint;
+	[SerializeField] private Transform m_target;
+
+	private float m_countdown = 5.0f;
+	#endregion
+
+	#region METODOS UNTIY
+	private void Update()
+	{
+		
+	}
+	#endregion
+
+	#region METODOS PROPIOS
+	
+	#endregion
+}
--- Assets/Scripts/Enemies/Boss/AI_BossPatrol.cs
+++ Assets/Scripts/Enemies/Boss/AI_BossPatrol.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class AI_BossPatrol : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	#region VARIABLES
+	[SerializeField] private float m_speed = 5.0f;
+	[SerializeField] private List<Transform> m_waypoints;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private float m_distanceToChange = 0.1f;
+	private byte m_index = 0;
+	#endregion
+
+	#region METODOS UNITY
+	private void Update()
+	{
+		MoveBoss();
+		GetNextWaypoint();
+	}
+	#endregion
+
+	#region METODOS PROPIOS
+	private void GetNextWaypoint()
+	{
+		if (Vector3.Distance(transform.position, m_waypoints[m_index].transform.position) < m_distanceToChange)
+		{
+			++m_index;
+			if (m_index >= m_waypoints.Count)
+			{
+				m_index = 0;
+			}
+		}
+	}
+	private void MoveBoss()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, m_waypoints[m_index].transform.position, m_speed * Time.deltaTime);
+	}
+	#endregion
 }
