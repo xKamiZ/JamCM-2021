2f6dd30da45b7cd4f6162abafa0c42555f61be77
Ivan0000001 <92449590+Ivan0000001@users.noreply.github.com>
2021-12-18 13:36:25 +0100

    Vida Player y Sprites Menu

--- Assets/Scripts/Others/Bullet.cs
+++ Assets/Scripts/Others/Bullet.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Bullet : MonoBehaviour
 {
 	#region VARIABLES
@@ -14,6 +14,7 @@ public class Bullet : MonoBehaviour
 	private GameObject m_target;
 	private Vector2 m_directionToTarget;
 	private bool m_canBounce = false;
+
 	#endregion
 
 	#region PROPIEDADES
@@ -29,6 +30,7 @@ public class Bullet : MonoBehaviour
 		m_target = GameObject.FindGameObjectWithTag("Target");
 		m_directionToTarget = (m_target.transform.position - transform.position).normalized * m_bulletForce;
 		m_rb2d.velocity = new Vector2(m_directionToTarget.x, m_directionToTarget.y);
+
 	}
     private void Update()
     {
@@ -49,7 +51,7 @@ public class Bullet : MonoBehaviour
         if (collision.tag == "Player")
         {
 			Debug.Log("Player daño");
-			Destroy(gameObject);
+			Destroy(collision.gameObject);
         }
 	}
 	#endregion
